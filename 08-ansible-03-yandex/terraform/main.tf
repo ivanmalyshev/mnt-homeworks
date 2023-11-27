149939ee84578860c56617cc7901542a09885b30
Ivan Malyshev <ivanmalyshev1994@gmail.com>
2023-11-27 23:49:31 +0300

    add ans-hw-03

---
+++ 08-ansible-03-yandex/terraform/main.tf
@@ -0,0 +1,46 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+
+data "yandex_compute_image" "ubuntu" {
+  #family = "ubuntu-2004-lts"
+  #family = var.vm_family
+  family = "centos-7"
+}
+resource "yandex_compute_instance" "vm-1" {
+  #name        = "netology-develop-platform-web"
+  name = local.name_web
+  #platform_id = "standard-v1"
+  platform_id = var.vm_web_platform_id
+
+  resources {
+    cores         = var.vm_web_resources["cores"]
+    memory        = var.vm_web_resources["memory"]
+    core_fraction = var.vm_web_resources["core_fraction"]
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("cloud-init.yaml")}"
+    serial-port-enable = "1"
+  }
+
+}
---
+++ 08-ansible-03-yandex/terraform/vms_platform.tf
@@ -0,0 +1,33 @@
+
+variable "vm_web_platform_id" {
+  type        = string
+  default     = "standard-v1"
+  description = "server-type"
+}
+
+variable "vm_db_platform_id" {
+  type        = string
+  default     = "standard-v1"
+  description = "server-type"
+}
+
+# VM web resources
+variable "vm_web_resources" {
+  type = map(number)
+  default = {
+    cores         = 4
+    memory        = 8
+    core_fraction = 5
+  }
+}
+
+# VM db resources
+variable "vm_db_resources" {
+  type = map(number)
+  default = {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+}
+
